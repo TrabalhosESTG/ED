757d3ce2e6ccfb9bfe5547baf929cbaafcb42d00
Guilherme Silva <guilhermeacrsilva@gmail.com>
2023-01-29 23:35:14 +0000

    change from kotlin to grovy

--- settings.gradle.kts
+++
@@ -1,1 +0,0 @@
-rootProject.name = "Grupo9ED"
--- src/main/java/pt/estg/Grupo9ED/IndexController.java
+++ src/main/java/pt/estg/Grupo9ED/IndexController.java
@@ -6,13 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class TesteWebController {
+public class IndexController {
 
-	@GetMapping("/teste")
-	public String teste(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+	@GetMapping("/")
+	public String index(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
-		return "TesteWeb";
+		return "index";
 	}
-
 }
 
